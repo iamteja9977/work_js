7719bcb04a65dafa11fd0d3e5c7f520d5c3dffce
iamteja9977 <thiru21ssc@gmail.com>
2022-09-02 17:45:26 +0530

    updated on 31

--- 200-Algo/1.js
+++
@@ -1,9 +0,0 @@
-// 1) Write a program to given input Check whether Even or odd.
-import readlineSync from "readline-sync"
-let a=readlineSync.questionInt("Enter the number : ")
-if(a%2==0){
-    console.log("you entered even number : ", a);
-}
-else{
-    console.log("you entered odd number : ", a);
-}
--- Async/5.js
+++
@@ -1,9 +0,0 @@
-console.log('Hi');
-for (var i = 0; i <= 5; i++) {
-    console.log(i);
-    setTimeout(() => {
-        console.log(i);
-    }, 5000);
-}
-console.log('Bye');
-console.log(i);
